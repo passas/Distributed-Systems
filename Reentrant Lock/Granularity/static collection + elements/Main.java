767532b8c5e1a299d39fa53d150dc9a2f6558580
Passinhas <48985218+passas@users.noreply.github.com>
2023-08-30 16:15:19 +0100

    [Granularity/...] scenario & test

---
+++ Reentrant Lock/Granularity/static collection + elements/Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+	private static final int N_THREADS = 2;
+	private static final int N_ACCOUNTS = 10;
+
+	public static void main (String[] args)
+	{
+		Thread ts[] = new Thread [ N_THREADS ];
+		Bank my_bank;
+
+		my_bank = new Bank ( N_ACCOUNTS );
+		//scenario simulation
+		for (int i=0; i<N_ACCOUNTS; i++)
+			my_bank.deposit (i, 1000.0);
+
+		for (int i=0; i<N_THREADS; i++)
+			ts[i] = new Thread ( new Bank_Transfer (my_bank, N_ACCOUNTS) );
+
+		for (int i=0; i<N_THREADS; i++)
+			ts[i].start();
+
+		for (int i=0; i<N_THREADS; i++)
+			try
+			{
+				ts[i].join();
+			} 
+			catch (Exception e)
+			{}
+
+		System.out.println ("Bank Total: "+my_bank.totalBalance());
+	}
+}
