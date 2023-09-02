895c0d85a6657c9e0f5439297784e8d2cfea7539
Passinhas <48985218+passas@users.noreply.github.com>
2023-09-02 19:42:03 +0100

    [multiple clients at a time - shared state] Server (newbie)

--- 4. Socket TCP/c) multiple clients at a time + shared state/Server.java
+++ 4. Socket TCP/c) multiple clients at a time + shared state/Server.java
@@ -5,20 +5,29 @@ public class Server
 {
 	public static void main (String[] args)
 	{
+		//Server:
+		ServerSocket server_socket;
 		Shared_State shared_state;
+		
+		//Attendence:
+		Socket socket;
+		Server_Runner runner;
+		Thread thread;
 
+		//Start shared object
 		shared_state = new Shared_State ();
 
 		try
-		{
-			ServerSocket server_socket = new ServerSocket ( 12345 );
-	
+		{ //Start server: wait for connections
+			server_socket = new ServerSocket ( 12345 );
+	        
 			while ( true )
 			{								
-				Socket socket = server_socket.accept ();
-			
-				Thread thread = new Thread ( new Server_Runner ( shared_state, socket ) );
-				thread.start();
+				socket = server_socket.accept (); // *waiting connection* -> establishing communication channel
+
+				runner = new Server_Runner ( shared_state, socket ); //create -thread- runner: send it the communication channel for the client & the server shared state obj.
+				thread = new Thread ( runner ); //create thread: send it the runner
+				thread.start(); //start thread: runner
 			}
 		}
 		catch (Exception e)
