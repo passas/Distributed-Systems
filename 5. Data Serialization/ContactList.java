1318486ca7555b3468b582fb06f9879fb9dc0307
Passinhas <48985218+passas@users.noreply.github.com>
2023-09-04 17:08:10 +0100

    List of Objects

--- 5. Data Serialization/ContactList.java
+++ 5. Data Serialization/ContactList.java
@@ -1,4 +1,4 @@
-/*import java.io.DataInputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,17 +6,25 @@ import java.util.ArrayList;
 class ContactList extends ArrayList<Contact>
 {
 
-    // @TODO
     public void serialize (DataOutputStream out) throws IOException
     {
+        out.writeInt ( this.size() );
         for (Contact c : this)
             c.serialize ( out );
     }
 
-    // @TODO
     public static ContactList deserialize (DataInputStream in) throws IOException
     {
+        int N;
+        ContactList contacts;
 
+        contacts = new ContactList ();
+
+        N = in.readInt();
+        for (int i=0; i<N; i++)
+            contacts.add ( Contact.deserialize (in) );
+
+        return contacts;
     }
 
-}*/
+}
