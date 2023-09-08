ee459591443f4c09ba01b16150dde5c4e044927e
Passinhas <48985218+passas@users.noreply.github.com>
2023-09-08 14:28:42 +0100

    Server Runner

---
+++ 6. Connection Manager/c) N-Threads per Multi-Threaded Client with 1 listener/Server_Runner.java
@@ -0,0 +1,56 @@
+public class Server_Runner implements Runnable
+{
+	private TaggedConnection connection;
+
+	public Server_Runner (TaggedConnection connection)
+	{
+		this.connection = connection;
+	}
+
+	public void run ()
+	{
+		TaggedConnection.Frame frame;
+		int tag; String text;
+
+		try
+		{
+			while ( (frame = this.connection.receive()) != null )
+			{
+				tag = frame.tag;
+				text = new String ( frame.data );
+
+				if ( tag == 0 )
+				{
+					System.out.println("Got one-way: " + text);
+				}
+				else if ( tag % 2 == 1 )
+				{
+					System.out.println("Replying to: " + text);
+                    this.connection.send(tag, text.toUpperCase().getBytes());
+				}
+				else if ( tag % 2 == 0 )
+				{
+					for (int i = 0; i < text.length(); ++i)
+					{
+                    	String str = text.substring(i, i+1);
+                        System.out.println("Streaming: " + str);
+                        this.connection.send(tag, str.getBytes());
+                    }
+                   	this.connection.send(tag, new byte[0]); //send byte STREAM terminator
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			try
+			{
+				this.connection.close ();
+			}
+			catch (Exception e2)
+			{
+				e2.printStackTrace();
+			}
+		}
+	}
+}
