316f4aabbb0e5c99515a53714d06e8615d382e21
Passinhas <48985218+passas@users.noreply.github.com>
2023-09-08 14:29:08 +0100

    Server

---
+++ 6. Connection Manager/c) N-Threads per Multi-Threaded Client with 1 listener/Server.java
@@ -0,0 +1,32 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    final static int WORKERS_PER_CONNECTION = 3;
+
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket server_socket;
+
+        Socket socket;
+        TaggedConnection connection;
+        Runnable worker;
+
+        server_socket = new ServerSocket ( 12345 );
+
+        while(true)
+        {
+            socket = server_socket.accept();
+            connection = new TaggedConnection( socket );
+
+            for (int i = 0; i < WORKERS_PER_CONNECTION; ++i)
+            {
+                worker = new Server_Runner ( connection );
+                new Thread(worker).start();
+            }
+        }
+
+    }
+}
+
